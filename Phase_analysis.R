71c1c8766142a102c6ced1c1827dcf8e0df8a2fe
NusretSalli <nusret.emirhan.salli@gmail.com>
2023-03-13 16:30:40 +0100

    updated

--- Phase_analysis.R
+++ Phase_analysis.R
@@ -61,44 +61,97 @@ output_int <- data.frame(output)
 
 output_int[,c(1,2,3,4)] <- lapply(output[,c(1,2,3,4)], function(x) ceiling(x))
 
-x_grid <- seq(1,max(output_int$N),length.out = length(output$N))
+x_grid <- seq(min(output_int$G),max(output_int$N),length.out = length(output$L))
 
-y_grid <- seq(1,max(output_int$G),length.out = length(output$G))
+y_grid <- seq(min(output_int$G),max(output_int$G),length.out = length(output$L))
+
+
+
+## WHAT UFFE WANTS ##
+
+ggplot(data = output, aes(x = N, y = G)) + geom_point(size = 3, color = "blue") + geom_line(color = "red", linewidth = 1.5) + 
+  labs(title = "Number of cells", x = "time", y = "number of cells")
+
+
+ggplot(data = output, aes(x = N, y = L)) + geom_point(size = 3, color = "blue") + geom_line(color = "red", linewidth = 1.5) + 
+  labs(title = "Number of cells", x = "time", y = "number of cells")
+
+ggplot(data = output, aes(x = G, y = L)) + geom_point(size = 3, color = "blue") + geom_line(color = "red", linewidth = 1.5) + 
+  labs(title = "Number of cells", x = "time", y = "number of cells")
+
+
+# when doing with different initial values #
+
+N0_list <- runif(10, min = 5, max = 50)
+
+G0_list <- runif(10, min = 200, max = 350)
+
+L0_list <- p$G_medium - N0_list - G0_list
+
+x0_list <- c(N = N0_list[3], G = G0_list[3], L = L0_list[3])
+
+
+sol_list <- ode(x0_list,time,final_model,p)
+
+output_list <- data.frame(sol_list)
+
+ggplot(data = output_list, aes(x = N, y = G)) + geom_point(size = 3, color = "blue") +
+  labs(title = "NG phaseplot", x = "N", y = "G")
+
+
+ggplot(data = output_list, aes(x = N, y = L)) + geom_point(size = 3, color = "blue") + 
+  labs(title = "NL phaseplot", x = "N", y = "L")
+
+ggplot(data = output_list, aes(x = G, y = L)) + geom_point(size = 3, color = "blue") + 
+  labs(title = "GL phaseplot", x = "G", y = "L")
+
+
+
 
 full_grid <- expand.grid(x_grid,y_grid)
 
 
-full_grid$N <- output$N
+full_grid$L <- output_int$L
 
 
 # output[,c(2,3,4)] <- lapply(output[,c(2,3,4)], function(x) ceiling(x))
 
+plot1 <- ggplot(full_grid, aes(x = Var1, y = Var2, z = L)) +
+  geom_contour_filled()
 
-plot_N_G <- ggplot(output, aes(x = N, y = G)) +
-  geom_point() +
-  xlim(-60, 370) +
-  ylim(-50, 470) +
-  stat_density_2d_filled(alpha = 0.5) +
-  stat_density_2d(linewidth = 0.25, colour = "black")
-  
 
-plot_N_G
+plot1
 
-plot_N_L <- ggplot(output, aes(x = N, y = L)) +
-  geom_point() +
-  xlim(-30, 370) +
-  ylim(-30, 120) +
-  stat_density_2d_filled(alpha = 0.5) +
-  stat_density_2d(linewidth = 0.25, colour = "black")
 
-plot_N_L
 
-plot_G_L <- ggplot(output, aes(x = L, y = G)) +
-  geom_point()+
-  stat_density_2d_filled(alpha=0.5)+
-  stat_density_2d(linewidth = 0.25, colour = "black")
 
-plot_G_L
+
+# 
+# plot_N_G <- ggplot(output, aes(x = N, y = G)) +
+#   geom_point() +
+#   xlim(-60, 370) +
+#   ylim(-50, 470) +
+#   stat_density_2d_filled(alpha = 0.5) +
+#   stat_density_2d(linewidth = 0.25, colour = "black")
+#   
+# 
+# plot_N_G
+# 
+# plot_N_L <- ggplot(output, aes(x = N, y = L)) +
+#   geom_point() +
+#   xlim(-30, 370) +
+#   ylim(-30, 120) +
+#   stat_density_2d_filled(alpha = 0.5) +
+#   stat_density_2d(linewidth = 0.25, colour = "black")
+# 
+# plot_N_L
+# 
+# plot_G_L <- ggplot(output, aes(x = L, y = G)) +
+#   geom_point()+
+#   stat_density_2d_filled(alpha=0.5)+
+#   stat_density_2d(linewidth = 0.25, colour = "black")
+# 
+# plot_G_L
 
 
 # N0_list <- runif(1000, 5, 50)
@@ -155,4 +208,24 @@ plot_G_L
 # )
 # 
 # fig
+
+# gg = ggplot(output_int, aes(x = N, y = G)) + 
+#   geom_raster(aes(fill = L)) + 
+#   geom_contour(aes(z = L), colour = "white", size = 0.2, alpha = 0.5) + 
+#   geom_text_contour(aes(z = L),  colour = "white" ) +
+#   labs(x = "Filtered light intensity (umol photons/m2/s)", 
+#        y = "Dry Biomass Concentration (g/L)", 
+#        fill = "Productivity (umol O2/g biomass/s)") + 
+#   theme(legend.title = element_text(size = 10, face = "bold"), 
+#         legend.position = "top", panel.background = element_blank(), 
+#         axis.text = element_text(colour = "black", size = 10, face = "bold"), 
+#         axis.title = element_text(size = 12, face = "bold"), 
+#         legend.text = element_text(size = 11), legend.key = element_blank()) + 
+#   scale_fill_continuous(low = "#BFE1B0", high = "#137177") + 
+#   scale_y_continuous(expand = c(0,0)) +
+#   scale_x_continuous(expand = c(0,0)) 
+# 
+# gg
+
+
 
