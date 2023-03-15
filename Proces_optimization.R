2faef4e7e1fa3adbabfe92a4a45f8cfb7a3a39fa
NusretSalli <nusret.emirhan.salli@gmail.com>
2023-03-15 19:19:52 +0100

    updated

--- Proces_optimization.R
+++ Proces_optimization.R
@@ -1,4 +1,35 @@
 
+rm(list=ls())
+
+require(deSolve)
+
+require(ggplot2)
+
+require(tidyr)
+
+require(phaseR)
+
+require(ODEsensitivity)
+
+require(epiR)
+
+require(FME)
+
+require(dplyr)
+
+require(plotly)
+
+source("Models.R")
+
+source("Phase_analysis_func.R")
+
+source("Sensitivity_analysis_func.R")
+
+#source("helper functions/3dplot.R")
+
+#source("helper functions/2dplot.R")
+
+source("Parameter_estimation_func.R")
 
 
 
